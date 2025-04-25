241ec1c5178fa3fff305a3bf3403790b1e97946e
Tran Viet Anh <tran@vietanh.cz>
2025-04-25 19:29:40 +0200

    Delete unnecessary things

--- supabase.ts
+++ supabase.ts
@@ -4,15 +4,6 @@ import { Database } from "./types/database.types";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
-
-if (!supabaseUrl) {
-  console.error('Supabase URL is not defined in environment variables');
-}
-
-if (!supabaseKey) {
-  console.error('Supabase Anon Key is not defined in environment variables');
-}
-
 export const supabase = createClient<Database>(
   supabaseUrl,
   supabaseKey
