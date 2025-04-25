ea92556f1997f0afc19d353d30ee94aabe5c0aec
Tran Viet Anh <tran@vietanh.cz>
2025-04-25 19:21:29 +0200

    add env vars to config

--- next.config.js
+++ next.config.js
@@ -30,6 +30,10 @@ const nextConfig = {
   compiler: {
     styledComponents: true
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 }
 
 module.exports = nextConfig
