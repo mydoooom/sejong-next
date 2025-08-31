0de24c00c44764c31ddc6acd6afc4e3dd5b467b9
Tran Viet Anh <tran@vietanh.cz>
2025-08-31 18:14:53 +0200

    Disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,30 +1,31 @@
 const path = require('path')
-const { i18n } = require('./next-i18next.config')
+const {i18n} = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  i18n,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  modularizeImports: {
-    '@mui/icons-material': {
-      transform: '/@mui/icons-material/{{member}}'
-    }
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-      },
-    ],
-  },
-  compiler: {
-    styledComponents: true
-  },
+    reactStrictMode: true,
+    i18n,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    modularizeImports: {
+        '@mui/icons-material': {
+            transform: '/@mui/icons-material/{{member}}'
+        }
+    },
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                port: '',
+            },
+        ],
+    },
+    compiler: {
+        styledComponents: true
+    },
 }
 
 module.exports = nextConfig
